709f0c8b3dee400325503a497633823aeb8a1075
OleksandrBodnarchuk <obodnarchuk@outlook.com>
2022-01-20 20:03:45 +0100

    Changed folders

--- src/main/java/pl/alex/jpa/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.alex.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.alex.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}
